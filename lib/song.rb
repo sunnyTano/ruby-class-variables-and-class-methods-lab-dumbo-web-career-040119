ce8abbabc64f2f3623cf3842ac4393dc06e79bd3
Susan Tan <ssusantan9@gmail.com>
2019-04-05 02:15:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,28 +29,28 @@ def self.genres
   @@genre.uniq
 end 
 
-def self.artist_count
-  artist_count = {}
-  @@artist.each do |artist|
-    if artist_count[artist]
-      artist_count[artist] += 1 
-    else
-      artist_count[artist] = 1
-    end
-  end 
-  artist_count
-end 
-#   def self.artist_count
-#     artist_count = {}
-#     @@artists.each do |artist|
-#       if artist_count[artist] #if the artist is already in the hash
-#         artist_count[artist] += 1
-#       else
-#         artist_count[artist] = 1
-#       end
+# def self.artist_count
+#   artist_count = {}
+#   @@artist.each do |artist|
+#     if artist_count[artist]
+#       artist_count[artist] += 1 
+#     else
+#       artist_count[artist] = 1
 #     end
-#     artist_count
-#   end
-# end
+#   end 
+#   artist_count
+# end 
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist] #if the artist is already in the hash
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
+end
 
 end 
