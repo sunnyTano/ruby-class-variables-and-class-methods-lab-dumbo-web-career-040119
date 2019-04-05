75601f3181b2d111719b21f4b8619ff81281eab6
Susan Tan <ssusantan9@gmail.com>
2019-04-05 02:22:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 
 class Song
   
@@ -31,10 +32,9 @@ end
 def self.genre_count
   genre_count = {}
   @@genres.each do |genre|
-    if genre_count[genre]
-      genre_count[genre] += 1 
+    genre_count = genre_count[genre] ? genre_count[genre] += 1 :   \\genre_count[genre] = 1 
     else
-      genre_count[genre] = 1 
+    
     end 
   end 
   genre_count
@@ -52,6 +52,4 @@ def self.artist_count
   artist_count
 end 
   
-
-
 end 
