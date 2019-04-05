357bb147c6012d5fc2e81a62cd1d02b8d6318d10
Susan Tan <ssusantan9@gmail.com>
2019-04-05 01:51:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -24,4 +24,19 @@ def self.artist
   @@artist << artist
 end 
 
+
+
+#   def self.artist_count
+#     artist_count = {}
+#     @@artists.each do |artist|
+#       if artist_count[artist] #if the artist is already in the hash
+#         artist_count[artist] += 1
+#       else
+#         artist_count[artist] = 1
+#       end
+#     end
+#     artist_count
+#   end
+# end
+
 end 
