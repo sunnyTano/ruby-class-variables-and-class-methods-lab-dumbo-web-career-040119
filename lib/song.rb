ebb41e8dbc037a44d0a415f657ec05b12c185e5d
Susan Tan <ssusantan9@gmail.com>
2019-04-05 02:13:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,8 +31,8 @@ end
 
 def self.artist_count
   artist_count = Hash.new
-  @@artist.map do |artist|
-    if artist_count[artist]
+  @@artist.each do |artist|
+    if artist_count[artist].length > 1
       artist_count[artist] += 1 
     else
       artist_count[artist] = 1
